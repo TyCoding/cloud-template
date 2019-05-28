583279798a32649db0be861a33c3ecbe9e120a03
tycoding <2557988481@qq.com>
2019-05-28 15:48:43 +0800

    :bookmark: init repo

--- sct-api/base.sql
+++ sct-api/base.sql
@@ -1,75 +1,136 @@
--- used in tests that use HSQL
-create table oauth_client_details
-(
-    client_id               VARCHAR(256) PRIMARY KEY,
-    resource_ids            VARCHAR(256),
-    client_secret           VARCHAR(256),
-    scope                   VARCHAR(256),
-    authorized_grant_types  VARCHAR(256),
-    web_server_redirect_uri VARCHAR(256),
-    authorities             VARCHAR(256),
-    access_token_validity   INTEGER,
-    refresh_token_validity  INTEGER,
-    additional_information  VARCHAR(4096),
-    autoapprove             VARCHAR(256)
-);
+DROP DATABASE IF EXISTS `sct`;
 
-create table oauth_client_token
-(
-    token_id          VARCHAR(256),
-    token             BLOB,
-    authentication_id VARCHAR(256) PRIMARY KEY,
-    user_name         VARCHAR(256),
-    client_id         VARCHAR(256)
-);
+CREATE DATABASE  `sct` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
-create table oauth_access_token
-(
-    token_id          VARCHAR(256),
-    token             BLOB,
-    authentication_id VARCHAR(256) PRIMARY KEY,
-    user_name         VARCHAR(256),
-    client_id         VARCHAR(256),
-    authentication    BLOB,
-    refresh_token     VARCHAR(256)
-);
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-create table oauth_refresh_token
-(
-    token_id       VARCHAR(256),
-    token          BLOB,
-    authentication BLOB
-);
+-- ----------------------------
+-- Table structure for ClientDetails
+-- ----------------------------
+DROP TABLE IF EXISTS `ClientDetails`;
+CREATE TABLE `ClientDetails` (
+  `appId` varchar(256) NOT NULL,
+  `resourceIds` varchar(256) DEFAULT NULL,
+  `appSecret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `grantTypes` varchar(256) DEFAULT NULL,
+  `redirectUrl` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additionalInformation` varchar(4096) DEFAULT NULL,
+  `autoApproveScopes` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`appId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table oauth_code
-(
-    code           VARCHAR(256),
-    authentication BLOB
-);
+-- ----------------------------
+-- Table structure for oauth_access_token
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_access_token`;
+CREATE TABLE `oauth_access_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(256) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  `authentication` blob,
+  `refresh_token` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table oauth_approvals
-(
-    userId         VARCHAR(256),
-    clientId       VARCHAR(256),
-    scope          VARCHAR(256),
-    status         VARCHAR(10),
-    expiresAt      TIMESTAMP NULL DEFAULT NULL,
-    lastModifiedAt TIMESTAMP NULL DEFAULT NULL
-);
+-- ----------------------------
+-- Table structure for oauth_approvals
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_approvals`;
+CREATE TABLE `oauth_approvals` (
+  `userId` varchar(256) DEFAULT NULL,
+  `clientId` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `expiresAt` timestamp NULL DEFAULT NULL,
+  `lastModifiedAt` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Table structure for oauth_client_details
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_client_details`;
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(256) NOT NULL,
+  `resource_ids` varchar(256) DEFAULT NULL,
+  `client_secret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `authorized_grant_types` varchar(256) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- customized oauth_client_details table
-create table ClientDetails
-(
-    appId                  VARCHAR(256) PRIMARY KEY,
-    resourceIds            VARCHAR(256),
-    appSecret              VARCHAR(256),
-    scope                  VARCHAR(256),
-    grantTypes             VARCHAR(256),
-    redirectUrl            VARCHAR(256),
-    authorities            VARCHAR(256),
-    access_token_validity  INTEGER,
-    refresh_token_validity INTEGER,
-    additionalInformation  VARCHAR(4096),
-    autoApproveScopes      VARCHAR(256)
-);
+-- ----------------------------
+-- Records of oauth_client_details
+-- ----------------------------
+BEGIN;
+INSERT INTO `oauth_client_details` VALUES ('client', NULL, '$2a$10$22emI3a6/w3a4ZZIa0.pY.dvLsyx4GH.he37wULtW8nJ.TeiGUpC6', 'app', 'password', 'http://tycoding.cn', NULL, NULL, NULL, NULL, NULL);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for oauth_client_token
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_client_token`;
+CREATE TABLE `oauth_client_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(256) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for oauth_code
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_code`;
+CREATE TABLE `oauth_code` (
+  `code` varchar(256) DEFAULT NULL,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for oauth_refresh_token
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(255) NOT NULL COMMENT '用户名',
+  `password` varchar(255) NOT NULL COMMENT '密码',
+  `salt` varchar(255) DEFAULT NULL COMMENT '随机盐',
+  `phone` varchar(255) DEFAULT NULL COMMENT '手机号',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_user` VALUES (1, 'tycoding', '123456', 'aaaaaaaa', '12311111', 'http://cdn.tycoding.cn/author.png', '2019-05-22 17:37:22');
+INSERT INTO `sys_user` VALUES (2, 'admin', '$2a$10$9ZhDOBp.sRKat4l14ygu/.LscxrMUcDAfeVOEPiYwbcRkoB09gCmi', 'aaaaaaaa', '12311111', 'http://cdn.tycoding.cn/author.png', '2019-05-24 13:07:45');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
